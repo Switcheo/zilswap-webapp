c2fb591ac2925761c9dd295c7736cdde01ad9424
Steven Khong <khongwh@gmail.com>
2020-03-25 23:43:02 +0800

    implement theme preference persistence

--- src/app/store/preference/reducer.ts
+++ src/app/store/preference/reducer.ts
@@ -1,8 +1,23 @@
-import { TYPES } from "./actions";
-import { PreferenceState } from "./types";
+import { TYPES, update } from "./actions";
+import { PreferenceState, PreferenceStateUpdateProps } from "./types";
+
+const LOCAL_STORAGE_KEY_THEME = "zilswap:theme";
+const VALID_THEMES = ["dark", "light"];
+
+const prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+const savedThemePreference = localStorage.getItem(LOCAL_STORAGE_KEY_THEME);
+const initialTheme = savedThemePreference || (prefersDarkMode ? "dark" : "light");
 
 const initial_state: PreferenceState = {
-  theme: "light",
+  theme: VALID_THEMES.includes(initialTheme) ? initialTheme : "light",
+};
+
+const checkToSaveThemePreference = (currentTheme: string, updatePayload: PreferenceStateUpdateProps) => {
+  const { theme } = updatePayload;
+  if (!theme) return;
+
+  if (theme !== currentTheme && VALID_THEMES.includes(theme))
+    localStorage.setItem(LOCAL_STORAGE_KEY_THEME, theme);
 };
 
 const reducer = (state: PreferenceState = initial_state, actions: any) => {
@@ -13,6 +28,7 @@ const reducer = (state: PreferenceState = initial_state, actions: any) => {
         ...actions.payload,
       };
     case TYPES.UPDATE:
+      checkToSaveThemePreference(state.theme, actions.payload);
       return {
         ...state,
         ...actions.payload,
@@ -20,8 +36,6 @@ const reducer = (state: PreferenceState = initial_state, actions: any) => {
     default:
       return state;
   };
-
-
 }
 
 export default reducer;
