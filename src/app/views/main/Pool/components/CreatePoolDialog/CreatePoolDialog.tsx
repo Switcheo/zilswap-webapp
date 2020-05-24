06fbc106f6c5a3744a5f1f9db9d42c1c68c63611
Steven Khong <khongwh@gmail.com>
2020-05-24 15:24:26 +0800

    abstract AddressInput from CreatePoolDialog

--- src/app/views/main/Pool/components/CreatePoolDialog/CreatePoolDialog.tsx
+++ src/app/views/main/Pool/components/CreatePoolDialog/CreatePoolDialog.tsx
@@ -1,12 +1,12 @@
-import { Box, DialogContent, InputLabel, makeStyles, OutlinedInput, Typography } from "@material-ui/core";
-import { DialogModal, FancyButton, KeyValueDisplay, LoadableArea } from "app/components";
+import { DialogContent, makeStyles } from "@material-ui/core";
+import { DialogModal, FancyButton } from "app/components";
 import { RootState, WalletState } from "app/store/types";
-import { useAsyncTask, truncate } from "app/utils";
 import cls from "classnames";
-import { zilParamsToMap } from "core/utilities";
-import { ZilliqaValidate, toBech32Address, ZilswapConnector } from "core/zilswap";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { AddressInput } from "./components";
+import { TokenPreview } from "./components/AddressInput/AddressInput";
+import { useAsyncTask } from "app/utils";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,81 +19,18 @@ const useStyles = makeStyles(theme => ({
       width: 296
     }
   },
-  input: {
-    marginBottom: 20,
-  },
-  inputError: {
-    border: `1px solid ${theme.palette.error.main}`
-  },
-  inputProps: {
-    [theme.breakpoints.down("xs")]: {
-      '&::placeholder': {
-        fontSize: "11px"
-      }
-    }
-  },
-  preview: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(6),
-  },
   actionButton: {
     height: 46,
     marginBottom: theme.spacing(6),
   },
-  error: {
-    float: "right",
-  },
-  floatLeft: {
-    float: "left",
-  },
 }));
-
-type TokenPreview = {
-  name: string;
-  symbol: string;
-  decimals: number;
-};
 
 const CreatePoolDialog = (props: any) => {
   const { children, className, open, onCloseDialog, onSelect, ...rest } = props;
   const classes = useStyles();
-  const [address, setAddress] = useState<string>("");
   const [tokenPreview, setTokenPreview] = useState<TokenPreview | undefined>();
-  const [runQueryTokenAddress, loadingQueryTokenAddress, errorQueryTokenAddress] = useAsyncTask("createPoolQueryTokenAddress");
+  const [runAsyncTask, loading, error] = useAsyncTask("createPool");
   const walletState = useSelector<RootState, WalletState>(state => state.wallet);
-  const [error] = useState("");
-
-  useEffect(() => {
-    if (loadingQueryTokenAddress) return;
-    let inputAddress = address;
-    if (ZilliqaValidate.isAddress(inputAddress))
-      inputAddress = toBech32Address(inputAddress);
-
-    if (ZilliqaValidate.isBech32(inputAddress)) {
-
-      runQueryTokenAddress(async () => {
-        if (!walletState.wallet) 
-          throw new Error("Connect wallet to view token information");
-
-        const zilliqa = ZilswapConnector.getZilliqa();
-        const contract = zilliqa.contracts.at(inputAddress);
-
-        const contractInitParams = await contract.getInit();
-        if (!contractInitParams)
-          throw new Error(`${truncate(address)} is not a contract address`);
-        const contractInit = zilParamsToMap(contractInitParams);
-
-        setTokenPreview({
-          name: contractInit.name,
-          symbol: contractInit.symbol,
-          decimals: parseInt(contractInit.decimals),
-        });
-
-      });
-    } else {
-      setTokenPreview(undefined);
-    }
-  }, [walletState.wallet, address]);
 
   const onCreatePool = () => {
 
@@ -102,29 +39,7 @@ const CreatePoolDialog = (props: any) => {
   return (
     <DialogModal header="Create Pool" open={open} onClose={onCloseDialog} {...rest} className={cls(classes.root, className)}>
       <DialogContent className={classes.content}>
-        <InputLabel className={classes.floatLeft}>Token Address</InputLabel>
-        {errorQueryTokenAddress && (
-          <InputLabel className={classes.error}><Typography color="error">{errorQueryTokenAddress.message}</Typography></InputLabel>
-        )}
-        <OutlinedInput
-          placeholder="Token Address"
-          value={address}
-          fullWidth
-          className={cls(classes.input, error ? classes.inputError : {})}
-          onChange={(e) => setAddress(e.target.value)}
-          inputProps={{ className: classes.inputProps }}
-        />
-        <Box className={classes.preview}>
-          <LoadableArea loading={loadingQueryTokenAddress}>
-            {!!tokenPreview && (
-              <>
-                <KeyValueDisplay kkey={"Name"} value={tokenPreview.name} mb="8px" />
-                <KeyValueDisplay kkey={"Symbol"} value={tokenPreview.symbol} mb="8px" />
-                <KeyValueDisplay kkey={"Decimals"} value={`${tokenPreview.decimals}`} mb="8px" />
-              </>
-            )}
-          </LoadableArea>
-        </Box>
+        <AddressInput onTokenChanged={preview => setTokenPreview(preview)} />
         <FancyButton walletRequired fullWidth
           disabled={!tokenPreview}
           className={classes.actionButton}
---
+++ src/app/views/main/Pool/components/CreatePoolDialog/components/AddressInput/AddressInput.tsx
@@ -0,0 +1,124 @@
+import { Box, BoxProps, InputLabel, makeStyles, OutlinedInput, Typography } from "@material-ui/core";
+import { KeyValueDisplay, LoadableArea } from "app/components";
+import { RootState, WalletState } from "app/store/types";
+import { truncate, useAsyncTask } from "app/utils";
+import cls from "classnames";
+import { zilParamsToMap } from "core/utilities";
+import { toBech32Address, ZilliqaValidate, ZilswapConnector } from "core/zilswap";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+export type TokenPreview = {
+  name: string;
+  symbol: string;
+  address: string;
+  decimals: number;
+};
+
+export interface AddressInputProps extends BoxProps {
+  onTokenChanged: (tokenPreview?: TokenPreview) => void;
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+  },
+  input: {
+    marginBottom: 20,
+  },
+  inputError: {
+    border: `1px solid ${theme.palette.error.main}`
+  },
+  inputProps: {
+    [theme.breakpoints.down("xs")]: {
+      '&::placeholder': {
+        fontSize: "11px"
+      }
+    }
+  },
+  preview: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(6),
+  },
+  error: {
+    float: "right",
+  },
+  floatLeft: {
+    float: "left",
+  },
+}));
+
+const AddressInput: React.FC<AddressInputProps> = (props: AddressInputProps) => {
+  const { children, className, onTokenChanged, ...rest } = props;
+  const classes = useStyles();
+  const [address, setAddress] = useState<string>("");
+  const [tokenPreview, setTokenPreview] = useState<TokenPreview | undefined>();
+  const [runAsyncTask, loading, error] = useAsyncTask("createPoolQueryTokenAddress");
+  const walletState = useSelector<RootState, WalletState>(state => state.wallet);
+
+  useEffect(() => {
+    if (typeof onTokenChanged === "function")
+      onTokenChanged(tokenPreview);
+  }, [tokenPreview]);
+
+  useEffect(() => {
+    if (loading) return;
+    let inputAddress = address;
+    if (ZilliqaValidate.isAddress(inputAddress))
+      inputAddress = toBech32Address(inputAddress);
+
+    if (ZilliqaValidate.isBech32(inputAddress)) {
+
+      runAsyncTask(async () => {
+        if (!walletState.wallet)
+          throw new Error("Connect wallet to view token information");
+
+        const zilliqa = ZilswapConnector.getZilliqa();
+        const contract = zilliqa.contracts.at(inputAddress);
+
+        const contractInitParams = await contract.getInit();
+        if (!contractInitParams)
+          throw new Error(`${truncate(address)} is not a contract address`);
+        const contractInit = zilParamsToMap(contractInitParams);
+
+        setTokenPreview({
+          name: contractInit.name,
+          symbol: contractInit.symbol,
+          address: inputAddress,
+          decimals: parseInt(contractInit.decimals),
+        });
+      });
+    } else {
+      setTokenPreview(undefined);
+    }
+  }, [walletState.wallet, address]);
+
+  return (
+    <Box {...rest} className={cls(classes.root, className)}>
+      <InputLabel className={classes.floatLeft}>Token Address</InputLabel>
+      {error && (
+        <InputLabel className={classes.error}><Typography color="error">{error.message}</Typography></InputLabel>
+      )}
+      <OutlinedInput
+        placeholder="Token Address"
+        value={address}
+        fullWidth
+        className={cls(classes.input, error ? classes.inputError : {})}
+        onChange={(e) => setAddress(e.target.value)}
+        inputProps={{ className: classes.inputProps }}
+      />
+      <Box className={classes.preview}>
+        <LoadableArea loading={loading}>
+          {!!tokenPreview && (
+            <>
+              <KeyValueDisplay kkey={"Name"} value={tokenPreview.name} mb="8px" />
+              <KeyValueDisplay kkey={"Symbol"} value={tokenPreview.symbol} mb="8px" />
+              <KeyValueDisplay kkey={"Decimals"} value={`${tokenPreview.decimals}`} mb="8px" />
+            </>
+          )}
+        </LoadableArea>
+      </Box>
+    </Box>
+  );
+};
+
+export default AddressInput;
