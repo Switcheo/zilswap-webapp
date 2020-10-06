373ebdad6c5fee7a8117edff54f265d2d7ca86fd
Steven Khong <khongwh@gmail.com>
2020-10-06 15:23:45 +0800

    add warning tool tip for alternate net mode

--- src/app/components/FancyButton/FancyButton.tsx
+++ src/app/components/FancyButton/FancyButton.tsx
@@ -1,12 +1,13 @@
-import { Box, Button, ButtonProps, CircularProgress, Tooltip } from "@material-ui/core";
+import { Box, Button, ButtonProps, CircularProgress, Tooltip, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { actions } from "app/store";
-import { RootState, WalletState } from "app/store/types";
+import { LayoutState, RootState, WalletState } from "app/store/types";
 import { useTaskSubscriber } from "app/utils";
 import { LoadingKeys } from "app/utils/contants";
 import cls from "classnames";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Network } from "zilswap-sdk/lib/constants";
 
 export interface FancyButtonProps extends ButtonProps {
   loading?: boolean;
@@ -34,12 +35,44 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     flexBasis: 150,
   },
+  altnetRibbon: {
+    width: "41px",
+    height: "41px",
+    overflow: "hidden",
+    position: "absolute",
+    top: "-1px",
+    left: "-1px",
+    // pointerEvents: "none",
+    "&>span": {
+      top: "10px",
+      left: "-15px",
+      width: "62px",
+      display: "block",
+      padding: "0 10px",
+      textOverflow: "ellipsis",
+      overflow: "hidden",
+      position: "relative",
+      transform: "rotate(-45deg)",
+      fontSize: "9px",
+      textAlign: "center",
+      boxShadow: "0px 0px 3px rgba(0,0,0,0.3)",
+      ...theme.palette.type === "dark" && {
+        color: "black",
+        backgroundColor: "#fff",
+      },
+      ...theme.palette.type === "light" && {
+        color: "white",
+        backgroundColor: "#333",
+      },
+    },
+  },
 }));
 const FancyButton: React.FC<FancyButtonProps> = (props: any) => {
   const { children, loading, className, walletRequired, disabled, loadingTxApprove, showTxApprove, onClickTxApprove, onClick, ...rest } = props;
   const classes = useStyles();
   const dispatch = useDispatch();
   const walletState = useSelector<RootState, WalletState>(state => state.wallet);
+  const layoutState = useSelector<RootState, LayoutState>(state => state.layout);
   const [loadingConnectWallet] = useTaskSubscriber(...LoadingKeys.connectWallet);
 
   const onButtonClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
@@ -82,6 +115,13 @@ const FancyButton: React.FC<FancyButtonProps> = (props: any) => {
         </Tooltip>
       )}
       <Button {...rest} disabled={buttonDisabled || showTxApprove} className={cls(classes.confirmButton, className)} onClick={onButtonClick}>
+        {layoutState.network !== Network.MainNet && (
+          <Tooltip placement="top-start" title={`You have selected ${layoutState.network}. Switch to MainNet for actual trade.`}>
+            <Box className={classes.altnetRibbon}>
+              <Typography component="span">{layoutState.network}</Typography>
+            </Box>
+          </Tooltip>
+        )}
         {!buttonLoading && buttonContent}
         {!!buttonLoading && (
           <CircularProgress size={24} className={classes.progress} />
--- src/app/utils/contants.ts
+++ src/app/utils/contants.ts
@@ -39,4 +39,4 @@ export const sortTokens = (lhs: TokenInfo, rhs: TokenInfo) => {
   return lhsPriority - rhsPriority;
 };
 
-export const ZIL_TOKEN_NAME =  "zil1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq9yf6pz";
+export const ZIL_TOKEN_NAME = "zil1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq9yf6pz";
