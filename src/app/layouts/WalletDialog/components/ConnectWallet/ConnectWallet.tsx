64a0a9020c5350a53a37156a7b7d786ca37cde0b
Steven Khong <khongwh@gmail.com>
2021-06-24 14:33:55 +0800

    Unhide Zeeves wallet integration

--- src/app/layouts/WalletDialog/components/ConnectWallet/ConnectWallet.tsx
+++ src/app/layouts/WalletDialog/components/ConnectWallet/ConnectWallet.tsx
@@ -6,13 +6,12 @@ import { AppTheme } from "app/theme/types";
 import { useSearchParam } from "app/utils";
 import cls from "classnames";
 import { ConnectOptionType } from "core/wallet";
-import React, { Fragment } from "react";
+import React from "react";
 import { ConnectWalletOption } from "./components";
 import { ReactComponent as PrivateKeyIconDark } from "./private-key-dark.svg";
 import { ReactComponent as PrivateKeyIcon } from "./private-key.svg";
-import { ReactComponent as ZilPayIcon } from "./zilpay.svg";
 import { ReactComponent as ZeevesIcon } from "./zeeves.svg";
-import { PRODUCTION_HOSTS } from "app/utils/constants";
+import { ReactComponent as ZilPayIcon } from "./zilpay.svg";
 
 export interface ConnectWalletProps {
   onSelectConnectOption: (option: ConnectOptionType) => void;
@@ -92,18 +91,14 @@ const ConnectWallet: React.FC<ConnectWalletProps & React.HTMLAttributes<HTMLDivE
             href="https://chrome.google.com/webstore/detail/zilpay/klnaejjgbibmhlephnhpmaofohgkpgkd">
             here
           </Link>.
-          {showZeevesOption && (
-            <Fragment>
-              <br />
-              <br />Or try{" "}
-              <Link rel="noopener noreferrer" target="_blank"
-                href="https://t.me/zilliqawalletbot">
-                Zeeves
-              </Link>,
-              {" "}
-              a Telegram-based wallet.
-            </Fragment>
-          )}
+          <br />
+          <br />Or try{" "}
+          <Link rel="noopener noreferrer" target="_blank"
+            href="https://t.me/zilliqawalletbot">
+            Zeeves
+          </Link>,
+          {" "}
+          a Telegram-based wallet.
         </Typography>
       </DialogContent>
     </Box>
