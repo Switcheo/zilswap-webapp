d1f8cdc61f9af9b0a27550fa469f570c90c4bd7c
Jereld Lim <jereldlimjy@hotmail.com>
2021-07-14 03:09:34 +0800

    added logos, chip

--- src/app/components/BridgeTransactionDialog/BridgeTransactionBox/BridgeTransactionBox.tsx
+++ src/app/components/BridgeTransactionDialog/BridgeTransactionBox/BridgeTransactionBox.tsx
@@ -1,12 +1,14 @@
-import { Box, Button, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { Box, Button, Chip, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@material-ui/core";
 import AddIcon from '@material-ui/icons/AddRounded';
 import ArrowRightRoundedIcon from '@material-ui/icons/ArrowRightRounded';
-import { HelpInfo, Reveal, Text } from 'app/components';
+import { CurrencyLogo, HelpInfo, Reveal, Text } from 'app/components';
 import { ReactComponent as NewLinkIcon } from "app/components/new_link.svg";
+import { actions } from "app/store";
 import { AppTheme } from "app/theme/types";
 import { hexToRGBA } from "app/utils";
 import cls from "classnames";
-import React from "react";
+import React, { Fragment } from "react";
+import { useDispatch } from "react-redux";
 
 const useStyles = makeStyles((theme: AppTheme) => ({
     root: {
@@ -23,6 +25,10 @@ const useStyles = makeStyles((theme: AppTheme) => ({
         [theme.breakpoints.down("xs")]: {
             minWidth: 320
         },
+        "& .MuiChip-root": {
+            borderRadius: 12,
+            color: theme.palette.primary.main
+        }
     },
     textColoured: {
         color: theme.palette.primary.dark
@@ -79,21 +85,68 @@ const useStyles = makeStyles((theme: AppTheme) => ({
         marginRight: "-4px"
     },
     button: {
-        borderRadius: 12
+        borderRadius: 12,
+    },
+    tableContainer: {
+        padding: theme.spacing(0, 0.5),
+        '&::-webkit-scrollbar': {
+            width: '0.4rem'
+        },
+        '&::-webkit-scrollbar-thumb': {
+            backgroundColor: `rgba${hexToRGBA(theme.palette.type === "dark" ? "#DEFFFF" : "#003340", 0.1)}`,
+            borderRadius: 12
+        },
+    },
+    failedChip: {
+        backgroundColor: "#FF5252"
+    },
+    ongoingChip: {
+        backgroundColor: "#FFDF6B"
+    },
+    completeChip: {
+        backgroundColor: "#00FFB0"
+    },
+    currencyLogo: {
+        height: "20px",
+        width: "20px",
+        marginLeft: theme.spacing(0.5),
+        marginRight: theme.spacing(0.5)
+    },
+    transferAmount: {
+        display: "inline-flex",
+        alignItems: "center"
     }
 }));
+
+const ZWAP_TOKEN_ADDRESS = "zil1p5suryq6q647usxczale29cu3336hhp376c627";
 
 const BridgeTransactionBox = (props: any) => {
     const { className} = props;
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     const rows = [
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 6, 24, 4.0, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 9.0, 37, 4.3, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 16.0, 24, 6.0, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 3.7, 67, 4.3, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 16.0, 49, 3.9, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 6.0, 24, 4.0, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 9.0, 37, 4.3, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 16.0, 24, 6.0, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 3.7, 67, 4.3, 1, 1],
+        ['22 Jul 2021', 'Ethereum - Zilliqa', 16.0, 49, 3.9, 1, 1],
         ['22 Jul 2021', 'Ethereum - Zilliqa', 6.0, 24, 4.0, 1, 1],
         ['22 Jul 2021', 'Ethereum - Zilliqa', 9.0, 37, 4.3, 1, 1],
         ['22 Jul 2021', 'Ethereum - Zilliqa', 16.0, 24, 6.0, 1, 1],
         ['22 Jul 2021', 'Ethereum - Zilliqa', 3.7, 67, 4.3, 1, 1],
         ['22 Jul 2021', 'Ethereum - Zilliqa', 16.0, 49, 3.9, 1, 1],
     ];
+
+    const handleNewTransfer = () => {
+        dispatch(actions.Layout.toggleShowBridgeTransactions("close"));
+        dispatch(actions.Layout.showTransferConfirmation(false));
+    }
 
     return (
         <Box overflow="hidden" display="flex" flexDirection="column" className={cls(classes.root, className)}>
@@ -109,15 +162,15 @@ const BridgeTransactionBox = (props: any) => {
                 </Box>
 
                 <Box display="flex" pt={0.5} pb={0.5}>
-                    <Button color="primary" variant="contained" className={classes.newTransferButton}>
+                    <Button color="primary" variant="contained" className={classes.newTransferButton} onClick={handleNewTransfer}>
                         <AddIcon fontSize="small" className={classes.addIcon} />
                         <Text variant="button" className={classes.textWhite}>New Transfer</Text>
                     </Button>
                 </Box>
             </Box>
 
-            <TableContainer>
-                <Table>
+            <TableContainer className={classes.tableContainer}>
+                <Table stickyHeader>
                     <TableHead className={classes.tableHead}>
                         <TableRow>
                             <TableCell>
@@ -132,13 +185,13 @@ const BridgeTransactionBox = (props: any) => {
                                     <Text>From - To</Text>
                                 </Box>
                             </TableCell>
-                            <TableCell align="right">
+                            <TableCell align="center">
                                 <Box display="flex" flexDirection="column">
                                     <Text variant="h6">Transfer</Text>
                                     <Text>Amount</Text>
                                 </Box>
                             </TableCell>
-                            <TableCell align="left">
+                            <TableCell align="center">
                                 <Box display="flex" flexDirection="column">
                                     <Text variant="h6">Transfer</Text>
                                     <Text>Status</Text>
@@ -170,10 +223,47 @@ const BridgeTransactionBox = (props: any) => {
                             <TableCell component="th" scope="row">
                                 {row[0]}
                             </TableCell>
-                            <TableCell>{row[1]}</TableCell>
-                            <TableCell align="right">{row[2]}</TableCell>
-                            <TableCell>{row[3]}</TableCell>
+                            <TableCell>
+                                <Text className={classes.transferAmount}>
+                                    <CurrencyLogo className={classes.currencyLogo} currency="ZWAP" address={ZWAP_TOKEN_ADDRESS}/>
+                                    Ethereum
+                                    <CurrencyLogo className={classes.currencyLogo} currency="ZWAP" address={ZWAP_TOKEN_ADDRESS}/>
+                                    Zilliqa
+                                </Text>
+                            </TableCell>
                             <TableCell align="center">
+                                <Text className={classes.transferAmount}>
+                                    {row[2]}
+                                    <CurrencyLogo className={classes.currencyLogo} currency="ZWAP" address={ZWAP_TOKEN_ADDRESS}/>
+                                </Text>
+                            </TableCell>
+                            <TableCell>
+                                <Chip
+                                    className={classes.ongoingChip}
+                                    label={
+                                        <Fragment>
+                                            <Typography align="center"><strong>Ongoing</strong></Typography>
+                                            <Typography align="center">Stage 1.2</Typography>
+                                        </Fragment>
+                                    }
+                                />
+                                {/* <Chip
+                                    className={classes.failedChip}
+                                    label={
+                                        <Fragment>
+                                            <Typography align="center"><strong>Failed</strong></Typography>
+                                            <Typography align="center">Stage 2.1</Typography>
+                                        </Fragment>
+                                    }
+                                /> */}
+                                {/* <Chip 
+                                    className={classes.completeChip}
+                                    label={
+                                        <Typography align="center"><strong>Complete</strong></Typography>
+                                    }
+                                /> */}
+                            </TableCell>
+                            <TableCell >
                                 <Reveal secret="12345" />
                             </TableCell>
                             <TableCell>
