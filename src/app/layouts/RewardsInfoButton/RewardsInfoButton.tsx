0746326e18aeb03c46fe3b451cca8500e3869c10
Jereld Lim <jereldlimjy@hotmail.com>
2021-07-29 21:24:28 +0800

    updates

--- src/app/layouts/RewardsInfoButton/RewardsInfoButton.tsx
+++ src/app/layouts/RewardsInfoButton/RewardsInfoButton.tsx
@@ -1,12 +1,14 @@
-import { Backdrop, Badge, Box, BoxProps, Button, Card, CircularProgress, ClickAwayListener, IconButton, Popper, Tooltip } from "@material-ui/core";
+import { Accordion, AccordionDetails, AccordionSummary, Backdrop, Badge, Box, BoxProps, Button, Card, CircularProgress, ClickAwayListener, IconButton, Link, Popper, Tooltip } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { CurrencyLogo, HelpInfo, KeyValueDisplay, Text } from "app/components";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDownRounded';
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
+import { CurrencyLogo, HelpInfo, Text } from "app/components";
 import { ReactComponent as NewLinkIcon } from "app/components/new_link.svg";
 import { actions } from "app/store";
 import { PendingClaimTx, RewardsState, RootState, TokenState, WalletState } from "app/store/types";
 import { AppTheme } from "app/theme/types";
-import { truncate, useAsyncTask, useNetwork, useValueCalculators } from "app/utils";
+import { hexToRGBA, truncate, useAsyncTask, useNetwork, useValueCalculators } from "app/utils";
 import { BIG_ZERO } from "app/utils/constants";
 import { formatZWAPLabel } from "app/utils/strings/strings";
 import BigNumber from "bignumber.js";
@@ -28,6 +30,21 @@ const useStyles = makeStyles((theme: AppTheme) => ({
     [theme.breakpoints.down("sm")]: {
       marginRight: theme.spacing(1),
     },
+    "& .MuiAccordion-root.Mui-expanded": {
+      backgroundColor: theme.palette.type === "dark" ? `rgba${hexToRGBA("#13222C", 0.5)}` : `rgba${hexToRGBA("#003340", 0.05)}`,
+      margin: 0
+    },
+    "& .MuiAccordionSummary-root": {
+      display: "inline-flex",
+      minHeight: "28px",
+      maxHeight: "28px"
+    },
+    "& .MuiAccordionDetails-root": {
+      display: "inherit",
+    },
+    "& .MuiAccordionSummary-content.Mui-expanded": {
+      margin: 0
+    }
   },
   backdrop: {
     zIndex: 1101,
@@ -50,22 +67,83 @@ const useStyles = makeStyles((theme: AppTheme) => ({
   buttonIcon: {
     marginLeft: theme.spacing(1),
   },
-  text: {
+  textColoured: {
     color: theme.palette.primary.dark
   },
   currencyLogo: {
     marginLeft: "2px",
   },
   currencyLogoSmall: {
-    height: "20px",
-    width: "20px"
+    height: "22px",
+    width: "22px"
   },
   tooltip: {
-    marginLeft: "4px"
+    marginLeft: "5px",
+    verticalAlign: "top!important",
   },
   claimRewardsButton: {
     padding: "16px",
   },
+  rewardBox: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    borderRadius: 10,
+    paddingTop: theme.spacing(1.5),
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    marginTop: theme.spacing(1)
+  },
+  dropDownIcon: {
+    color: theme.palette.primary.light
+  },
+  accordion: {
+    width: "100%",
+    borderRadius: "10px!important",
+    boxShadow: "none",
+    border: "none",
+    backgroundColor: "transparent",
+    "& .MuiIconButton-root": {
+      padding: 0,
+      marginRight: 0
+    },
+    "&:hover": {
+      backgroundColor: theme.palette.type === "dark" ? `rgba${hexToRGBA("#13222C", 0.5)}` : `rgba${hexToRGBA("#003340", 0.05)}`,
+    },
+  },
+  reward: {
+    display: "inline-flex",
+    alignItems: "flex-end",
+    marginBottom: theme.spacing(0.5)
+  },
+  currency: {
+   fontWeight: 600,
+   marginLeft: "2px"
+  },
+  link: {
+    color: theme.palette.text?.primary
+  },
+  linkIcon: {
+    marginLeft: theme.spacing(1),
+    marginBottom: "2px",
+    "& path": {
+      fill: theme.palette.text?.secondary,
+    }
+  },
+  body: {
+    fontSize: "14px",
+    fontWeight: "normal"
+  },
+  textSuccess: {
+    color: theme.palette.primary.dark
+  },
+  successIcon: {
+    verticalAlign: "top",
+  },
+  amount: {
+    fontFamily: "Avenir Next"
+  }
 }));
 
 const RewardsInfoButton: React.FC<Props> = (props: Props) => {
@@ -80,6 +158,7 @@ const RewardsInfoButton: React.FC<Props> = (props: Props) => {
   const [active, setActive] = useState(false);
   const [claimResult, setClaimResult] = useState<any>(null);
   const [claimCount, setClaimCount] = useState(0);
+  const [showDetails, setShowDetails] = useState<boolean>(false);
   const [runClaimRewards, loading, error] = useAsyncTask("claimRewards");
   const buttonRef = useRef();
   const theme = useTheme();
@@ -242,18 +321,60 @@ const RewardsInfoButton: React.FC<Props> = (props: Props) => {
             <Card className={classes.card}>
               <Box display="flex" flexDirection="column" alignItems="center">
                 <Text variant="h6" color="textPrimary">Your Balance</Text>
-                <Box display="flex" marginTop={2}>
-                  <Text variant="h2" className={classes.text}>
+                <Box display="flex" marginTop={1}>
+                  <Text variant="h2" className={classes.textColoured}>
                     {zapBalanceLabel}
                   </Text>
                   <CurrencyLogo currency="ZWAP" address={zwapAddress} className={classes.currencyLogo}/>
                 </Box>
-                <Text variant="h6" marginTop={0} className={classes.text}>
+                <Text variant="h6" marginTop={0} className={classes.textColoured}>
                   ≈ {zapTokenValue.toFormat(2)} USD
                 </Text>
               </Box>
 
-              <KeyValueDisplay marginTop={2} alignItems="center" emphasizeValue kkey="Rewards This Epoch">
+              {/* Rewards claimable */}
+              <Box display="flex" flexDirection="column" alignItems="center" mt={1.5}>
+                <Text className={classes.body}>
+                  Rewards Claimable
+                  <HelpInfo placement="bottom" title="The estimated amount of ZWAP you have earned but have not claimed." className={classes.tooltip}/>
+                </Text>
+                <Box className={classes.rewardBox} bgcolor="background.contrast" width="100%">
+                  <Text variant="h4" className={classes.reward}>
+                    {unclaimedRewardsLabel}
+                    <CurrencyLogo currency="ZWAP" address={zwapAddress} className={cls(classes.currencyLogo, classes.currencyLogoSmall)}/>
+                    <span className={classes.currency}>
+                      ZWAP
+                    </span>
+                  </Text>
+
+                  <Text variant="h4" className={classes.reward}>
+                    {unclaimedRewardsLabel}
+                    <CurrencyLogo currency="ZWAP" address={zwapAddress} className={cls(classes.currencyLogo, classes.currencyLogoSmall)}/>
+                    <span className={classes.currency}>
+                      ZWAP
+                    </span>
+                  </Text>
+
+                  <Text marginBottom={1} variant="body2" color="textSecondary" className={classes.amount}>
+                    ≈ $400.00
+                  </Text>
+
+                  <Accordion className={classes.accordion} expanded={showDetails} onChange={(_, expanded) => setShowDetails(expanded)}>
+                    <Box display="flex" justifyContent="center" width="100%">
+                      <AccordionSummary expandIcon={<ArrowDropDownIcon className={classes.dropDownIcon} />}>
+                        <Text color="textSecondary">View Details</Text>
+                      </AccordionSummary>
+                    </Box>
+                    <AccordionDetails>
+                      <Box>
+                        <h1>hello world</h1>
+                      </Box>
+                    </AccordionDetails>
+                  </Accordion>
+                </Box>
+              </Box>
+
+              {/* <KeyValueDisplay marginTop={2} alignItems="center" emphasizeValue kkey="Rewards This Epoch">
                 <Box display="flex" alignItems="center">
                   <Text variant="body2" color="textPrimary">
                     ≈ {potentialRewardsLabel}
@@ -273,8 +394,9 @@ const RewardsInfoButton: React.FC<Props> = (props: Props) => {
                     <HelpInfo placement="bottom" title="The estimated amount of ZWAP you have earned but have not claimed." className={classes.tooltip}/>
                   </Box>
                 </Badge>
-              </KeyValueDisplay>
+              </KeyValueDisplay> */}
 
+              {/* Need to change? */}
               {!!error && (
                 <Box marginTop={1}>
                   <Text variant="body1" color="error">
@@ -290,7 +412,7 @@ const RewardsInfoButton: React.FC<Props> = (props: Props) => {
                 </Box>
               )}
 
-              <Box marginTop={3} />
+              <Box marginTop={2} />
 
               {!claimResult && (
                 <Tooltip title={claimTooltip}>
@@ -304,10 +426,26 @@ const RewardsInfoButton: React.FC<Props> = (props: Props) => {
               )}
 
               {!!claimResult && (
-                <Button fullWidth variant="outlined" color="primary" target="_blank" href={`https://viewblock.io/zilliqa/tx/0x${claimResult?.hash}?network=${network?.toLowerCase()}`}>
-                  View Claim TX <NewLinkIcon className={classes.buttonIcon} />
-                </Button>
+                <Box display="flex" flexDirection="column" alignItems="center"> 
+                  <Text marginTop={2} variant="h4" className={classes.textSuccess}>
+                    <CheckCircleRoundedIcon fontSize="inherit" className={classes.successIcon} />
+                    {" "}
+                    Reward claims successful!
+                  </Text>
+                  <Link
+                    className={classes.link}
+                    underline="none"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={`https://viewblock.io/zilliqa/tx/0x${claimResult?.hash}?network=${network?.toLowerCase()}`}>
+                      <Box display="flex" justifyContent="center" alignItems="center" mt={0.5}>
+                        <Text className={classes.body}>View on Viewblock</Text>
+                        <NewLinkIcon className={classes.linkIcon} />
+                      </Box>
+                  </Link>
+                </Box>
               )}
+  
             </Card>
           </ClickAwayListener>
         </Box>
