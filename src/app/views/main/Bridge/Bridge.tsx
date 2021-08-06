317d3820677e53e669106ed6ffdf3320dcca0f11
Jereld Lim <jereldlimjy@hotmail.com>
2021-08-06 12:06:27 +0800

    bridge network select bg using keyboard

--- src/app/views/main/Bridge/Bridge.tsx
+++ src/app/views/main/Bridge/Bridge.tsx
@@ -101,7 +101,10 @@ const useStyles = makeStyles((theme: AppTheme) => ({
     "& .MuiListItem-root": {
       borderRadius: "12px",
       padding: theme.spacing(1.5),
-      justifyContent: "center"
+      justifyContent: "center",
+    },
+    "& .MuiListItem-root.Mui-focusVisible": {
+      backgroundColor: theme.palette.type === "dark" ? `rgba${hexToRGBA("#DEFFFF", 0.08)}` : "rgba(0, 0, 0, 0.04)",
     },
     "& .MuiListItem-root.Mui-selected": {
       backgroundColor: theme.palette.label,
