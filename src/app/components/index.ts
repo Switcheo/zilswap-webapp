cc15d08cc0e411075761e05da17e5dad3e03a335
Steven Khong <khongwh@gmail.com>
2021-08-27 15:02:09 +0800

    Refactor file export grouping

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -36,6 +36,7 @@ export { default as SlippageField } from "./SlippageField";
 export { default as StatefulText } from "./StatefulText";
 export { default as StatsCard } from "./StatsCard";
 export { default as Text } from "./Text";
+export { default as TokenFilter } from "./TokenFilter";
 export { default as TokenGraph } from "./TokenGraph";
 export { default as TopBar } from "./TopBar";
 export { default as UserPoolMessage } from "./UserPoolMessage";
--- src/app/views/pools/PoolTransactions/PoolTransactions.tsx
+++ src/app/views/pools/PoolTransactions/PoolTransactions.tsx
@@ -1,8 +1,7 @@
 import { Box, BoxProps, Checkbox, CircularProgress, Container, FormControlLabel, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Pagination } from "@material-ui/lab";
-import { PoolsNavigationTabs, PoolsOverviewBanner, Text } from "app/components";
-import TokenFilter from "app/components/TokenFilter";
+import { PoolsNavigationTabs, PoolsOverviewBanner, Text, TokenFilter } from "app/components";
 import Page from "app/layouts/Page";
 import { RootState, WalletState } from "app/store/types";
 import { AppTheme } from "app/theme/types";
