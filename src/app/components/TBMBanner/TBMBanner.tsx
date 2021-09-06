7693b86b0cbeb2b7eff354aaf3bcb6ed0e48152b
Melvin Beemer <melvin@hey.com>
2021-09-06 21:04:22 +0700

    Open in new tab

--- src/app/components/TBMBanner/TBMBanner.tsx
+++ src/app/components/TBMBanner/TBMBanner.tsx
@@ -22,7 +22,7 @@ const TBMBanner = () => {
   const classes = useStyles();
 
   return <Box className={classes.banner} maxWidth={800} margin="0 auto">
-    <a href="https://thebear.market">
+    <a href="https://thebear.market" target="_blank" rel="noreferrer">
       <img className={classes.bannerImage} src="/assets/tbm-banner.png" alt="" />
     </a>
   </Box>
