bd5906be999f1bd5208d4c6962994a1fa784d257
jx-willdo <wjunxing@hotmail.com>
2021-09-24 18:33:44 +0800

    remove tbm, should be using ark api

--- src/app/saga/app/marketplaceSaga.ts
+++ src/app/saga/app/marketplaceSaga.ts
@@ -1,33 +1,22 @@
 import { actions } from "app/store";
-import { fork, take, call, put, select } from "redux-saga/effects";
+import { fork, take, call, put } from "redux-saga/effects";
 import { logger } from "core/utilities";
-import { getWallet } from "../selectors";
-import * as  tbmConnector from "core/zilswap/tbm";
 import store from "app/store";
-
-// function* loadMarketPlace() {
-
-// }
 
 function* loadProfile() {
   try {
     yield put(actions.Layout.addBackgroundLoading("loadProfile", "LOAD_PROFILE"));
-    const { wallet } = getWallet(yield select())
-    const tokenIds: string[] = yield tbmConnector.getOwnedToken(wallet);
-    logger("update token id", tokenIds);
-    yield put(actions.MarketPlace.updateProfile({ ownedNft: tokenIds }));
+    // Load profile info with ark API
   } catch (error) {
     console.error("loading profile failed, Error:")
     console.error(error)
     setTimeout(() => {
       store.dispatch(actions.MarketPlace.loadProfile());
     }, 10000)
+  } finally {
+    yield put(actions.Layout.removeBackgroundLoading("LOAD_PROFILE"));
   }
 }
-
-// function* watchMarketPlaceInit() {
-
-// }
 
 function* watchProfileLoad() {
   while (true) {
@@ -39,6 +28,5 @@ function* watchProfileLoad() {
 
 export default function* marketPlaceSaga() {
   logger("init marketplace saga");
-  // yield fork(watchMarketPlaceInit);
   yield fork(watchProfileLoad);
 }
