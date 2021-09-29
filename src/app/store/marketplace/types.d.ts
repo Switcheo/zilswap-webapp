6c6ba4850fd7daad87ba20a9ab40b1dd484251e5
Steven Khong <khongwh@gmail.com>
2021-09-29 11:57:34 +0800

    Update NFT API structs

--- src/app/store/marketplace/types.d.ts
+++ src/app/store/marketplace/types.d.ts
@@ -2,60 +2,55 @@ import { SimpleMap } from "app/utils";
 import BigNumber from "bignumber.js";
 
 export interface MarketPlaceState {
-  collections: SimpleMap<Collection>,
-  tokens: SimpleMap<Nft>,
-  filter: {},
-  profile?: Profile,
+  collections: SimpleMap<Collection>;
+  tokens: SimpleMap<Nft>;
+  filter: {};
+  profile?: Profile;
 }
 
 export interface Nft {
-  id: string,
-  name?: string,
-  token_id: number,
-  collection_id: string,
-  owner_id: string,
-  description?: string,
-  metadata?: string,
-  asset_id?: string,
-  asset?: Asset,
-  collection?: Collection,
-  trait_values?: TraitValue[]
+  name?: string;
+  token_id: number;
+  description?: string;
+  metadata?: string;
+  asset?: Asset;
+  owner?: MarketplaceUser;
+  collection?: Collection;
+  trait_values?: TraitValue[];
 }
 
 export type TraitType = {
-  id: string,
-  trait: string,
-  collection_id: string,
+  trait: string;
+  collection?: Collection;
 }
 export interface TraitValue {
-  id: string,
-  value: string,
-  count: number,
-  trait_type_id: string,
-  trait_type: TraitType,
+  value: string;
+  count: number;
+  trait_type?: TraitType;
 }
 export interface Collection {
-  id: string,
-  name: string,
-  description: string,
-  address: string,
-  verified_at: string,
-  website_url: string,
-  discord_url: string,
-  telegram_url: string,
-  twitter_url: string,
-  instagram_url: string,
+  name: string;
+  description: string;
+  address: string;
+  verified_at: string;
+  website_url: string;
+  discord_url: string;
+  telegram_url: string;
+  twitter_url: string;
+  instagram_url: string;
 }
 export interface Asset {
-  id: string,
-  type: string,
-  mime_type: string,
-  filename: string,
-  url: string,
-  host: string,
-  content_length?: number,
+  type: string;
+  mime_type: string;
+  filename: string;
+  url: string;
+  content_length?: number;
 }
 
+export type MarketplaceUser = {
+  username?: string;
+  address: string;
+}
 
 export interface NftAttribute {
   trait_type: string;
@@ -64,10 +59,10 @@ export interface NftAttribute {
 }
 
 export interface Profile {
-  ownedNft: SimpleMap<Nft>,
-  biddedNft?: SimpleMap<BiddedNft>
+  ownedNft: SimpleMap<Nft>;
+  biddedNft?: SimpleMap<BiddedNft>;
 }
 
 export interface BiddedNft {
-  nft: Nft,
+  nft: Nft;
 }
